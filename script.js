ea9438a4e25efc03b6bd700e79545b133d9061f4
sshumail <56185708+sshumail@users.noreply.github.com>
2026-07-18 23:37:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('cta-btn').addEventListener('click', function () {
+  const msg = document.getElementById('click-msg');
+  msg.textContent = "You just clicked a button on a live website — nice.";
+});
