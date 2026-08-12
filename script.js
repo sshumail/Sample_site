efc42b8d2207476f4125c6657ed20a4633293bba
sshumail <56185708+sshumail@users.noreply.github.com>
2026-08-12 21:30:19 +0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('cta-btn').addEventListener('click', function () {
-  const msg = document.getElementById('click-msg');
-  msg.textContent = "You just clicked a button on a live website — nice.";
-});
